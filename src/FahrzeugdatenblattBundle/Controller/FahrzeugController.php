7fbb6ac552c1e6600d41ddb671933db613d754f8
TheWhykiki <info@whykiki.de>
2017-11-07 23:21:45 +0100

    - setup test controller fpr mapping cars blocked/unblocked

--- src/FahrzeugdatenblattBundle/Controller/FahrzeugController.php
+++ src/FahrzeugdatenblattBundle/Controller/FahrzeugController.php
@@ -3,6 +3,7 @@
 namespace FahrzeugdatenblattBundle\Controller;
 
 
+use FahrzeugdatenblattBundle\Entity\Transferfahrten;
 use FahrzeugdatenblattBundle\Form\FahrzeugdatenForm;
 use FahrzeugdatenblattBundle\Form\TransferfahrtenForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -78,25 +79,11 @@ class FahrzeugController extends Controller
 		]);
 	}
 
-	/**
-	 * @Route("/spielereikiki/{namekiki}", name="spielereiKiki")
-	 */
-	public function spielereikikiAction($namekiki){
 
-		$helptext = [
-			'Hallo1',
-			'Hallo2',
-			'Hallo3'
-		];
-		return $this->render('fahrzeugdatenblatt_frontend/ausgabe2.html.twig', [
-			'name' => $namekiki,
-			'help' => $helptext
-		]);
-	}
 
 	/************************************************************************************************************************
 	 *
-	 * Fahrten Testcontroller
+	 * Fahrten Testcontroller1
 	 *
 	 ***********************************************************************************************************************/
 
@@ -108,10 +95,91 @@ class FahrzeugController extends Controller
 
 		// Count all cars
 		$em = $this->getDoctrine()->getManager();
+
+
+
+		$fahrten = $em->getRepository('FahrzeugdatenblattBundle:Transferfahrten')
+						->getAllCarID();
+
 		$cars = $em->getRepository('FahrzeugdatenblattBundle:Fahrzeugdatenblatt')
-			->anfrageFilterCars();
+			->anfrageNeuAllCars();
+
+		dump($fahrten);die;
+		return $this->render('admin_templates/ausgabe_alle_fahrten_Alter.html.twig',[
+			'alleAutos' => $cars
+		]);
+	}
+
+	/************************************************************************************************************************
+	 *
+	 * Fahrten Testcontroller2
+	 *
+	 ***********************************************************************************************************************/
+
+	/**
+	 * @Route("/fahrten_testcontroller2", name="fahrten_testcontroller2")
+	 */
+	public function fahrtenTest2Action(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
 
 
+		// Find all cars from database
+
+		/** @var  Fahrzeugdatenblatt $getAllCars */
+		$getAllCars = $em->getRepository('FahrzeugdatenblattBundle:Fahrzeugdatenblatt')
+						->findAll();
+
+		// Set array for all Car IDs
+
+		$allCarIDs = [];
+		foreach ($getAllCars as $car){
+			/** @var  Fahrzeugdatenblatt $car */
+			$allCarIDs[] = $car->getID();
+		}
+
+		// Find all transfers which are blocked in the desired transfertime
+
+		/** @var  Transferfahrten $getBlockedTransfers */
+		$getBlockedTransfers = $em->getRepository('FahrzeugdatenblattBundle:Transferfahrten')
+			->getBlockedTransfers();
+
+
+		// Set array for all blocked Car IDs
+
+		$allBlockedCarIDs = [];
+
+		foreach($getBlockedTransfers as $transfer){
+			/** @var  Transferfahrten $transfer */
+			$allBlockedCarIDs [] = $transfer->getFahrzeuge()->getID();
+		}
+
+		// Set array for all Cars and State
+
+		$allCarsAndState = [];
+		foreach($getAllCars as $car){
+
+			// Set default state to false
+			$state = false;
+			/** @var  Fahrzeugdatenblatt $car */
+
+			// Get all car ids and compare if they
+			// are in $allBlockedCarIDs
+			// if they are in array set state to true
+			$carID = $car->getId();
+			if (in_array($carID, $allBlockedCarIDs)) {
+				 $state = true;
+			};
+
+			// Set id's and state
+
+			$allCarsAndState[] = [
+				'id' => $carID,
+				'state' => $state
+			];
+		}
+
+		dump($allCarsAndState);die;
 		return $this->render('admin_templates/ausgabe_alle_fahrten_Alter.html.twig',[
 			'alleAutos' => $cars
 		]);
